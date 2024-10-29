06af3b350b8071d82de88595ed803c3aad241a0b
STEVE KURIA <146070392+steve20146@users.noreply.github.com>
2024-10-29 17:21:15 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Future_FSWD_03
-E-COMMERCE RESPONSIVE ADMIN DASHBOARD IN HTML, CSS AND JAVASCRIPT
+# admin-dashboard
+Responsive Admin Dashboard Using HTML CSS &amp; JavaScript with Light &amp; Dark Mode
---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "ETHERNET CABLE",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "DATA CABLE",
+    productNumber: "36378",
+    paymentStatus: "Refund",
+    shipping: "Declined",
+  },
+  {
+    productName: "SMART WATCH",
+    productNumber: "49347",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "BLUETOOTH",
+    productNumber: "96996",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "NETWORK SWITCH",
+    productNumber: "22821",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "CHARGER TYPE C",
+    productNumber: "81475",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "TV SCREEN",
+    productNumber: "00482",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
